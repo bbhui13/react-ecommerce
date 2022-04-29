4db7eecb151299812a3fd04f50d749f640a9d752
Hui Hon Yu Bryan <bryanhuihy@gmail.com>
2022-04-29 23:47:15 +0800

    Homepage Navbar + 1 Product

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAHPjactPjsKpD_V8SRrVTPK8kuOSI3Pbc",
+    authDomain: "clone-3d178.firebaseapp.com",
+    projectId: "clone-3d178",
+    storageBucket: "clone-3d178.appspot.com",
+    messagingSenderId: "459864318463",
+    appId: "1:459864318463:web:201415b36844dd3849c7ca",
+    measurementId: "G-3047YJ22D9"
+  };
